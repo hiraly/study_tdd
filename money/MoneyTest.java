0898c32d927d0c87728dc6f11c1025f5abdfb72a
Koudai Hiradate <kodai.hiradate@zozo.com>
2022-01-05 17:21:21 +0900

    最初のtestに記述漏れがあった

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -1,11 +1,14 @@
 package money;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
     }
 }
