f9b427ad1603cc9826f57958b62a2398a69e22f8
Koudai Hiradate <kodai.hiradate@zozo.com>
2022-01-05 17:14:16 +0900

    最初のテストコード。 コンパイルエラーのあるコードをそのままコミットするのはなんとなくむずがゆい。 なんなら解決できないimportはIDEが自動で削除するw。"save without formatting"で保存した。

---
+++ MoneyTest.java
@@ -0,0 +1,11 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+    }
+}
